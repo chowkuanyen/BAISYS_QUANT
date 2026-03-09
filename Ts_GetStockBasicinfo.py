3d95ef0792bee8812f6f6aaa9a6a6a7e688bc909
Chowkuan Yen <yenchowkuan@gmail.com>
2026-03-09 15:29:22 +0100

    Update Ts_GetStockBasicinfo.py

--- Ts_GetStockBasicinfo.py
+++ Ts_GetStockBasicinfo.py
@@ -1,5 +1,4 @@
 import tushare
-
 import   tushare as ts
 import pandas as pd
 
@@ -15,7 +14,7 @@ class TushareStockManager:
         ts.set_token(token)
         self.pro = ts.pro_api()
 
-    def get_basic_data(self, list_status='L', save_path='stock_basic_data.txt') -> pd.DataFrame:
+    def get_basic_data(self, list_status='L',market ='主板', save_path='stock_basic_data.txt') -> pd.DataFrame:
         """
         获取股票基础数据，并保存到本地
         :param list_status: 上市状态 (L上市, D退市, P暂停上市)
@@ -24,14 +23,24 @@ class TushareStockManager:
         """
         try:
             # 定义需要的字段
-            target_fields = 'symbol,name,industry,market'
+            target_fields = 'ts_code,symbol,name,industry,market'
 
             # 从接口拉取数据
             df = self.pro.stock_basic(
                 exchange='',
                 list_status=list_status,
+                market =market,
                 fields=target_fields
             )
+
+            def format_ts_code(x):
+                if not isinstance(x, str) or '.' not in x:
+                    return x
+                code, market = x.split('.')
+                return market + code
+
+            df['ts_code'] = df['ts_code'].apply(format_ts_code)
+
 
             if df.empty:
                 print("⚠️ 警告：获取到的数据为空，请检查权限或参数。")
